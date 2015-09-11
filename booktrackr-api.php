9bfa6805ae8ee48d2d38587e5e2cdc18929d1e3f
Drew Butler <hi@dbtlr.com>
2015-09-11 02:52:05 -0400

    Update the book tracker to have custom post type book and to enable CORS properly.

--- booktrackr-api.php
+++ booktrackr-api.php
@@ -19,6 +19,11 @@ if ( ! defined( 'JSON_API_VERSION' ) ) {
 	error_log( 'BookTrackr API requires the REST JSON API plugin be loaded.', E_ERROR );
 }
 
+// Fix the fact that Magic quotes is on by default and BREAKS THE FREAKING OAUTH PLUGIN. WTF.
+add_action('init', function() {
+	$_GET = stripslashes_deep( $_GET );
+});
+
 register_post_type( 'book', array(
 	'labels' => array(
 		'name'          => __( 'Books', 'booktrackr' ),
@@ -31,35 +36,19 @@ register_post_type( 'book', array(
 	'rewrite' => false,
 	'query_var' => false,
 	'delete_with_user' => true,
-	'supports' => array( 'title', 'editor', 'author', 'thumbnail', 'page-attributes', 'custom-fields', 'comments' ),
+	'show_in_rest' => true,
+	'rest_base' => 'books',
+	'rest_controller_class' => 'WP_REST_Posts_Controller',
+	'supports' => array( 'title', 'comments', 'editor', 'featured_image' ),
 ) );
 
-register_post_type( 'book_highlight', array(
-	'labels' => array(
-		'name'          => __( 'Highlights', 'booktrackr' ),
-		'singular_name' => __( 'Book',  'booktrackr' ),
-	),
-	'public' => false,
-	'capability_type' => 'post',
-	'map_meta_cap' => true,
-	'hierarchical' => true,
-	'rewrite' => false,
-	'query_var' => false,
-	'delete_with_user' => true,
-	'supports' => array( 'title', 'editor', 'author', 'page-attributes', 'custom-fields', 'comments' ),
-) );
+// Make sure cross origin requests are allowed.
+// this is more insecure than you would typically use
+// in a production environment.
 
-register_post_type( 'book_review', array(
-	'labels' => array(
-		'name'          => __( 'Books', 'booktrackr' ),
-		'singular_name' => __( 'Book',  'booktrackr' ),
-	),
-	'public' => false,
-	'capability_type' => 'post',
-	'map_meta_cap' => true,
-	'hierarchical' => true,
-	'rewrite' => false,
-	'query_var' => false,
-	'delete_with_user' => true,
-	'supports' => array( 'title', 'editor', 'author', 'page-attributes', 'custom-fields', 'comments' ),
-) );
+add_filter( 'rest_pre_serve_request', function() {
+	if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+		header( "Access-Control-Allow-Origin: *" );
+		header( "Access-Control-Allow-Headers: Authorization,Content-Type" );
+	}
+}, 20); // We use a 20 so that this overrides the origin in the wp-api plugin which only works against self.
--- docs/Gulpfile.js
+++
@@ -1,9 +0,0 @@
-
-var raml2html = require('gulp-raml2html');
-var gulp = require('gulp');
-
-gulp.task('apidoc', function() {
-  return gulp.src('api.raml')
-      .pipe(raml2html())
-      .pipe(gulp.dest('.'));
-});
